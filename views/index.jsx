f730bd82f4c81d48ac4828644bfec3a754786dbe
Isaac Rodriguez <isaac.bh.rod@gmail.com>
2022-06-14 10:57:31 -0400

    sending index and show data

--- views/index.jsx
+++ views/index.jsx
@@ -10,7 +10,7 @@ const Index = ({breads}) => {
             {
                 breads.map((bread, index)=> {
                     return (<li key={index}>
-                     <a>
+                     <a href={`/breads/${index}`}>
                         {bread.name}
                      </a>   
                     </li>)
