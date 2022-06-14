3b4a05bbe56734134cd17048f988dcd12e836070
Isaac Rodriguez <isaac.bh.rod@gmail.com>
2022-06-14 10:54:22 -0400

    added bullets with bread names onto index page

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -1,12 +1,18 @@
-const express = require('express')
-const breads = express.Router()
-const Bread = require("../models/bread.js")
-//index
-breads.get('/', (req,res) => {
-    res.send(Bread)
-} )
-breads.get("/:arrayIndex", (req,res) => {
-    res.send(Bread[req.params.arrayIndex])
-})
+const express = require("express");
+const breads = express.Router();
+const Bread = require("../models/bread.js");
 
- module.exports = breads
+//index
+breads.get("/", (req, res) => {
+  res.render("Index",
+  {
+    breads: Bread
+  }
+  );
+  //res.send(Bread)
+});
+breads.get("/:arrayIndex", (req, res) => {
+  res.send(Bread[req.params.arrayIndex]);
+});
+
+module.exports = breads;
--- server.js
+++ server.js
@@ -6,6 +6,11 @@ require("dotenv").config();
 const PORT = process.env.PORT;
 const app = express();
 
+//middlelware
+app.set("views", __dirname + "/views");
+app.set("view engine", "jsx");
+app.engine("jsx", require("express-react-views").createEngine());
+
 //routes
 app.get("/", (req, res) => {
   res.send("Welcome to an awesom website about Breads!");
