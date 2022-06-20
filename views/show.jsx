bb1954d93abfbd10e44a4bfef4c628a42811f675
Isaac Rodriguez <isaac.bh.rod@gmail.com>
2022-06-20 13:46:04 -0400

    added edit and update routes, edit landing page not working

--- views/show.jsx
+++ views/show.jsx
@@ -15,8 +15,10 @@ const Show = ({ bread, index }) => {
       <img src={bread.image} alt={bread.name} />
       <form action={"/breads/${index}?_method=DELETE"} method="POST">
         <input type="submit" value="DELETE" />
-
       </form>
+      <a href={`/breads/${index}/edit`}>
+        <button>Edit</button>
+      </a>
       <li>
         <a href="/breads">DONKEY! Get out of my swamp!</a>
       </li>
