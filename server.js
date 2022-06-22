0772c43570b5ce68c81262f6b1d742bac4f0dce1
Isaac Rodriguez <isaac.bh.rod@gmail.com>
2022-06-22 19:15:59 -0400

    mongoose added, new schema

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 //dependencies
 const express = require("express");
 const methodOverride = require("method-override");
+const mongoose = require("mongoose")
 
 //configuration
 require("dotenv").config();
 const PORT = process.env.PORT;
 const app = express();
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
+  () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
+)
+
 
 //middlelware
 app.set("views", __dirname + "/views");
