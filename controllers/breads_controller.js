309a5c04d31b0aab02c4765f22fb4cafc476d1e0
Isaac Rodriguez <isaac.bh.rod@gmail.com>
2022-06-22 20:33:58 -0400

    updated show route, but default image is not working

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -4,13 +4,12 @@ const Bread = require("../models/bread.js");
 
 //index
 breads.get("/", (req, res) => {
-  Bread.find()
-  .then(foundBreads => {
+  Bread.find().then((foundBreads) => {
     res.render("Index", {
       breads: foundBreads,
       title: "Index Page",
     });
-  })
+  });
 
   //res.send(Bread)
 });
@@ -18,15 +17,14 @@ breads.get("/", (req, res) => {
 // create
 breads.post("/", (req, res) => {
   if (!req.body.image) {
-    req.body.image =
-      "https://images.unsplash.com/photo-1517686469429-8bdb88b9f907?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80";
+    req.body.image = undefined;
   }
   if (req.body.hasGluten === "on") {
     req.body.hasGluten = true;
   } else {
     req.body.hasGluten = false;
   }
-  Bread.push(req.body);
+  Bread.create(req.body);
   res.redirect("/breads");
 });
 
@@ -61,15 +59,16 @@ breads.put("/:arrayIndex", (req, res) => {
 });
 
 //show
-breads.get("/:arrayIndex", (req, res) => {
-  if (Bread[req.params.arrayIndex]) {
-    res.render("Show", {
-      bread: Bread[req.params.arrayIndex],
-      index: req.params.arrayIndex,
+breads.get("/:id", (req, res) => {
+  Bread.findById(req.params.id)
+    .then((foundBread) => {
+      res.render("show", {
+        bread: foundBread,
+      });
+    })
+    .catch((err) => {
+      res.send("404");
     });
-  } else {
-    res.send("404");
-  }
 });
 
 module.exports = breads;
--- models/bread.js
+++ models/bread.js
@@ -7,7 +7,7 @@ const { Schema } = mongoose;
 const breadSchema = new Schema({
   name: { type: String, required: true },
   hasGluten: Boolean,
-  image: { type: String, default: "http://placehold.it/500x500.png" },
+  image: { type: String, default: "https://images-gmi-pmc.edge-generalmills.com/6ef7a9a1-08dc-4e94-8de9-10c4002103a5.jpg" },
 });
 
 // model and export
